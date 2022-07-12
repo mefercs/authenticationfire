d492261ab5a1b56c5ecd4aee47e69ff41417c6d9
MeferxUpiita <mcruzs2103@alumno.ipn.mx>
2022-07-12 07:22:09 -0500

    firebase installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
